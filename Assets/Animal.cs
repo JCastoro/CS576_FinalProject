84b8af59e2e0900e4ea92397f53babc588bb3f01
Dennis Minn <minndennis@gmail.com>
2022-12-14 17:53:41 -0500

    minor code cleanup

--- Assets/Animal.cs
+++ Assets/Animal.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+enum AnimatorState {
+    Idle = 0,
+    Walking = 1,
+    Running = 2,
+    Eating = 3,
+    Dieing = 4,
+    Attacking = 5
+}
 
 public class Animal : MonoBehaviour{
-
+    // TODO Convert to Pascal naming convention
     private Vector3 Direction;
     private Vector3 Velocity;
     private Vector3 CurrentDestination;
-    private protected string FoodPreference;
+    protected string FoodPreference;
 
     private float FleeDistance;
 
-
     //navmesh testing
-    private protected NavMeshAgent agent;
+    protected NavMeshAgent agent;
     private float ArrivalTime = -1f;
     [Range(0,100)] public float speed;
     [Range(10,300)] public float ScavengeAreaMinX;
@@ -26,72 +33,48 @@ public class Animal : MonoBehaviour{
     private bool isWalkingToFood = false;
     private GameObject currFood = null;
  
-
-
-    //sphere colider
+    // Detection Sphere
     private SphereCollider SphereOfAwareness;
-    //animator
-    private protected Animator animation_controller;
 
-    // Start is called before the first frame update
-
-    //Animator Notes
-    /*
-        0 | Idle
-        1 | Walking
-        2 | Running
-        3 | Eating
-        4 | Dieing
-        5 | Attacking
-    */
-
+    // Animator Notes (check AnimatorState)
+    protected Animator animation_controller;
 
     protected virtual void Start(){
-
         agent = GetComponent<NavMeshAgent>();
-        agent.stoppingDistance=0f;
+        agent.stoppingDistance = 0f;
+        agent.speed = speed;
+        agent.SetDestination(RandomNavMeshLocationGRID());
 
         animation_controller = GetComponent<Animator>();
+
         SphereOfAwareness = GetComponent<SphereCollider>();
-        if(agent != null){
-            agent.speed = speed;
-            agent.SetDestination(RandomNavMeshLocationGRID());
-            animation_controller.SetInteger("AnimationState",1);
-        }
-
-
-
-        
     }
-    // Update is called once per frame
+
     protected virtual void Update(){
         if(Time.time < 1f)
             return;
-        // foreach(AnimatorControllerParameter param in animation_controller.parameters){
-        //     animation_controller.SetBool(param.name,false);
-        // }
-        if(agent.remainingDistance <= agent.stoppingDistance){//if at our currentDestination
 
+        if(agent.remainingDistance <= agent.stoppingDistance){//if at our currentDestination
             //flag to see if we have first arrived somewhere
             if (ArrivalTime == -1f){
                 ArrivalTime = Time.time;
                 Debug.Log("Arrival Time: " + (ArrivalTime));
-            } else if(Time.time - ArrivalTime > 5f){//if waiting time is over, set new destination
-                //Debug.Log("Time passed: " + (Time.time - ArrivalTime));
+            } else if(Time.time - ArrivalTime > 5f){
+                //if waiting time is over, set new destination
                 agent.SetDestination(RandomNavMeshLocationGRID());
-                animation_controller.SetInteger("AnimationState",1);
+                animation_controller.SetInteger("AnimationState", (int) AnimatorState.Walking);
                 StartCoroutine("arrivalDelay"); 
-            }
-            else{//do while waiting
-                if(isWalkingToFood & currFood!=null){// if we are walking to food
-                    Debug.Log("eating food " );
+            } else{
+                //do while waiting
+                if(isWalkingToFood && currFood!=null){// if we are walking to food
+                    Debug.Log("eating food");
                     StartCoroutine("Eating",currFood);
                 }
             }
         }
 
         else{
-            animation_controller.SetInteger("AnimationState",1);
+            animation_controller.SetInteger("AnimationState", (int) AnimatorState.Walking);
         }
     }
 
@@ -101,60 +84,42 @@ public class Animal : MonoBehaviour{
         float randomPositionX = Random.Range(ScavengeAreaMinX,ScavengeAreaMaxX);
         float randomPositionZ = Random.Range(ScavengeAreaMinY,ScavengeAreaMaxY);
         Vector3 randomPosition = new Vector3(randomPositionX,transform.position.y,randomPositionZ);
-        //Debug.Log(randomPosition);
 
         return randomPosition;
     }  
-    private void OnTriggerEnter(Collider other){
 
-        if(other.gameObject.tag == FoodPreference){//if animal sees the type of food it likes
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag == FoodPreference){
+            // Checks for food preference
             agent.SetDestination(other.gameObject.transform.position);
             isWalkingToFood=true;
             currFood = other.gameObject;
-
-        }
-        else if(other.gameObject.tag == "Player"&& other.gameObject.GetComponent<Player>().isCrouching == true){//will need to check for if player is crouching
+        } else if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<Player>().isCrouching == true){
+            // Checks for player presence (i.e. not crouching)
             Flee(other.gameObject.transform.position);
         }
     }
 
-    // triggers eat animation and deletes food game object upon completion
-    // animal will keep eating if it detects player. this is a feature.
-    public void Eat(GameObject Food){// make this a coroutine? whatever the hell that is....
-        // Walks to food
-        // If distance between animal and food < 0.5m trigger eat animation and destroy plant
-        Debug.Log("eating food " );
-        animation_controller.SetInteger("AnimationState",3);
-        Destroy(Food);
-    }
-
     IEnumerator arrivalDelay(){
+        Debug.Log("coroutine arrival delay");
         yield return new WaitForSeconds(0.05f);
         Debug.Log("delay over");
         ArrivalTime = -1f; 
     }
 
     IEnumerator Eating(GameObject Food){
-        Debug.Log("eating food " );
-        animation_controller.SetInteger("AnimationState",3);
+        Debug.Log("coroutine eating food" );
+        animation_controller.SetInteger("AnimationState", (int) AnimatorState.Eating);
         yield return new WaitForSeconds(2f);
         Destroy(Food);
     }
 
 
-    // run away from player
     public void Flee(Vector3 CurrPlayerPosition){
-        // animal keeps running until set distance away from player.
-            //could use this to control difficulty in game, harder animals run further away??
+        // TODO: add option to make animals flee further away
+        // TODO: check edge of map
         agent.speed = 2*agent.speed;
-        animation_controller.SetInteger("AnimationState",2);
-
-
-        //could check if we are near edge of map here so that character will run 
-        //normalized direciton away from player
+        animation_controller.SetInteger("AnimationState", (int) AnimatorState.Running);
         Vector3 fleeDirection = (CurrPlayerPosition - agent.transform.position).normalized;
-
-        //test this spot and move to it if its on the navmesh
-
     }
 }
