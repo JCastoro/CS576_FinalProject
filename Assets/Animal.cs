ee47e82d30924c6e23638cfdac488243981808f4
Dennis Minn <minndennis@gmail.com>
2022-12-15 14:09:17 -0500

    changed variable names to Pascal Case and parameters to Camel Case

--- Assets/Animal.cs
+++ Assets/Animal.cs
@@ -13,16 +13,13 @@ enum AnimatorState {
 }
 
 public class Animal : MonoBehaviour{
-    // TODO Convert to Pascal naming convention
+    // TODO Convert to public variable speed Pascal naming convention
     private Vector3 Direction;
     private Vector3 Velocity;
-    private Vector3 CurrentDestination;
     protected string FoodPreference;
 
-    private float FleeDistance;
-
     //navmesh testing
-    protected NavMeshAgent agent;
+    protected NavMeshAgent Agent;
     private float ArrivalTime = -1f;
     [Range(0,100)] public float speed;
     [Range(10,300)] public float ScavengeAreaMinX;
@@ -30,60 +27,58 @@ public class Animal : MonoBehaviour{
     [Range(10,300)] public float ScavengeAreaMaxX;
     [Range(10,300)] public float ScavengeAreaMaxY;
 
-    private bool isWalkingToFood = false;
-    private GameObject currFood = null;
+    private bool IsWalkingToFood = false;
+    private GameObject CurrFood = null;
  
     // Detection Sphere
-    private SphereCollider SphereOfAwareness;
+    private SphereCollider DetectionSphere;
 
     // Animator Notes (check AnimatorState)
-    protected Animator animation_controller;
+    protected Animator AnimationController;
 
     protected virtual void Start(){
-        agent = GetComponent<NavMeshAgent>();
-        agent.stoppingDistance = 0f;
-        agent.speed = speed;
-        agent.SetDestination(RandomNavMeshLocationGRID());
+        Agent = GetComponent<NavMeshAgent>();
+        Agent.stoppingDistance = 0f;
+        Agent.speed = speed;
+        Agent.SetDestination(RandomNavMeshLocationGRID());
 
-        animation_controller = GetComponent<Animator>();
-
-        SphereOfAwareness = GetComponent<SphereCollider>();
+        AnimationController = GetComponent<Animator>();
+        DetectionSphere = GetComponent<SphereCollider>();
     }
 
     protected virtual void Update(){
         if(Time.time < 1f)
             return;
 
-        if(agent.remainingDistance <= agent.stoppingDistance){//if at our currentDestination
+        if(Agent.remainingDistance <= Agent.stoppingDistance){//if at our currentDestination
             //flag to see if we have first arrived somewhere
             if (ArrivalTime == -1f){
                 ArrivalTime = Time.time;
                 Debug.Log("Arrival Time: " + (ArrivalTime));
             } else if(Time.time - ArrivalTime > 5f){
                 //if waiting time is over, set new destination
-                agent.SetDestination(RandomNavMeshLocationGRID());
-                animation_controller.SetInteger("AnimationState", (int) AnimatorState.Walking);
+                Agent.SetDestination(RandomNavMeshLocationGRID());
+                AnimationController.SetInteger("AnimationState", (int) AnimatorState.Walking);
                 StartCoroutine("arrivalDelay"); 
             } else{
                 //do while waiting
-                if(isWalkingToFood && currFood!=null){// if we are walking to food
+                if(IsWalkingToFood && CurrFood!=null){// if we are walking to food
                     Debug.Log("eating food");
-                    StartCoroutine("Eating",currFood);
+                    StartCoroutine("Eating", CurrFood);
                 }
             }
-        }
-
-        else{
-            animation_controller.SetInteger("AnimationState", (int) AnimatorState.Walking);
+        } else{
+            AnimationController.SetInteger("AnimationState", (int) AnimatorState.Walking);
         }
     }
 
+    // Gets a random point in 3d space within a sphere the size of our WalkRadius
     public Vector3 RandomNavMeshLocationGRID(){
-        Vector3 finalPosition = Vector3.zero;
-        //gets a random point in 3d space within a sphere the size of our WalkRadius
-        float randomPositionX = Random.Range(ScavengeAreaMinX,ScavengeAreaMaxX);
-        float randomPositionZ = Random.Range(ScavengeAreaMinY,ScavengeAreaMaxY);
-        Vector3 randomPosition = new Vector3(randomPositionX,transform.position.y,randomPositionZ);
+        Vector3 randomPosition = new Vector3(
+                Random.Range(ScavengeAreaMinX,ScavengeAreaMaxX),
+                transform.position.y,
+                Random.Range(ScavengeAreaMinY,ScavengeAreaMaxY)
+        );
 
         return randomPosition;
     }  
@@ -91,9 +86,9 @@ public class Animal : MonoBehaviour{
     private void OnTriggerEnter(Collider other){
         if(other.gameObject.tag == FoodPreference){
             // Checks for food preference
-            agent.SetDestination(other.gameObject.transform.position);
-            isWalkingToFood=true;
-            currFood = other.gameObject;
+            Agent.SetDestination(other.gameObject.transform.position);
+            IsWalkingToFood=true;
+            CurrFood = other.gameObject;
         } else if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<Player>().isCrouching == true){
             // Checks for player presence (i.e. not crouching)
             Flee(other.gameObject.transform.position);
@@ -107,19 +102,19 @@ public class Animal : MonoBehaviour{
         ArrivalTime = -1f; 
     }
 
-    IEnumerator Eating(GameObject Food){
+    IEnumerator Eating(GameObject food){
         Debug.Log("coroutine eating food" );
-        animation_controller.SetInteger("AnimationState", (int) AnimatorState.Eating);
+        AnimationController.SetInteger("AnimationState", (int) AnimatorState.Eating);
         yield return new WaitForSeconds(2f);
-        Destroy(Food);
+        Destroy(food);
     }
 
 
     public void Flee(Vector3 CurrPlayerPosition){
         // TODO: add option to make animals flee further away
         // TODO: check edge of map
-        agent.speed = 2*agent.speed;
-        animation_controller.SetInteger("AnimationState", (int) AnimatorState.Running);
-        Vector3 fleeDirection = (CurrPlayerPosition - agent.transform.position).normalized;
+        Agent.speed = 2*Agent.speed;
+        AnimationController.SetInteger("AnimationState", (int) AnimatorState.Running);
+        Vector3 fleeDirection = (CurrPlayerPosition - Agent.transform.position).normalized;
     }
 }
