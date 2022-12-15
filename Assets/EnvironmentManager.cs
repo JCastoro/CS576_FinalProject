6ca4913fd8029380264ca10ce9b1e5e2e8bcdfe1
Dennis Minn <minndennis@gmail.com>
2022-12-15 18:32:06 -0500

    Implemented randomly generated bear food

--- Assets/EnvironmentManager.cs
+++ Assets/EnvironmentManager.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnvironmentManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class EnvironmentManager : MonoBehaviour{
+    public GameObject BearFood;
+    
+    // TODO Add tree
+    void Start(){
+       GenerateGameObject(BearFood, 10, 0, 300, 0, 300); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void GenerateGameObject(GameObject gameObject, int numObjects, float minX, float maxX, float minZ, float maxZ){
+        for(int i=0; i<numObjects; ++i){
+            Vector3 GameObjectPosition = new Vector3(
+                    Random.Range(minX, maxX),
+                    255f, // maxY?
+                    Random.Range(minZ, maxZ)
+            );
+
+            GameObject InstantiatedGameObject = (GameObject) Instantiate(
+                    gameObject,
+                    GameObjectPosition,
+                    Quaternion.identity
+            );
+
+            Ray GameObjectRay = new Ray(InstantiatedGameObject.transform.position, -InstantiatedGameObject.transform.up);
+            RaycastHit EnvironmentPosition;
+
+            if (Physics.Raycast(GameObjectRay, out EnvironmentPosition) && EnvironmentPosition.collider.tag == "Terrain"){
+                InstantiatedGameObject.transform.position = new Vector3(
+                        EnvironmentPosition.point.x,
+                        EnvironmentPosition.point.y,
+                        EnvironmentPosition.point.z
+                );
+            }
+        }
+
     }
 }
