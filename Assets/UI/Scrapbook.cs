13434501d335914ec632946da98aff417e84e08a
Julian Castoro <jcastoro@umass.edu>
2022-12-18 16:47:28 -0500

    added footprint and food guides for scrapbook

--- Assets/UI/Scrapbook.cs
+++ Assets/UI/Scrapbook.cs
@@ -9,6 +9,7 @@ public class Scrapbook : MonoBehaviour
     public GameObject AnimalPanels;
     // Start is called before the first frame update
     void Start(){
+        MarkAnimalFound(0);
 
     }
 
@@ -20,11 +21,13 @@ public class Scrapbook : MonoBehaviour
     public void MarkAnimalFound(int AnimalIndex){
             GameObject AnimalPanel = AnimalPanels.transform.GetChild(AnimalIndex).gameObject;
             Image Image = AnimalPanel.transform.Find("ProfilePic").GetComponent<Image>();
+            Image FoodImage = AnimalPanel.transform.Find("Food").Find("foodImage").GetComponent<Image>();
             AnimalPanel.GetComponentInChildren<Text>().text = "FOUND";
             //adjusting PP
             Color TempColor = Image.color;
             TempColor.a = 0.4f;
             Image.color = TempColor;
+            FoodImage.color=TempColor;
     }
 
 }
